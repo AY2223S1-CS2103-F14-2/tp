667fd0784d04661f196814eda03912cc3426e006
lulucopter <lzydaniel.lee@gmail.com>
2022-10-10 06:33:40 +0800

    Create GraduationDate Class

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -2,13 +2,7 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_GENDER;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_UNIVERSITY;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -37,6 +31,7 @@ public class EditCommandParser implements Parser<EditCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
                     PREFIX_ADDRESS,
                     PREFIX_GENDER,
+                    PREFIX_GRADUATION_DATE,
                     PREFIX_UNIVERSITY,
                     PREFIX_TAG);
 
@@ -68,6 +63,11 @@ public class EditCommandParser implements Parser<EditCommand> {
         if (argMultimap.getValue(PREFIX_GENDER).isPresent()) {
             editPersonDescriptor
                 .setGender(ParserUtil.parseGender(argMultimap.getValue(PREFIX_GENDER).get()));
+        }
+        if (argMultimap.getValue(PREFIX_GRADUATION_DATE).isPresent()) {
+            editPersonDescriptor
+                    .setGraduationDate(ParserUtil.parseGraduationDate(
+                            argMultimap.getValue(PREFIX_GRADUATION_DATE).get()));
         }
         if (argMultimap.getValue(PREFIX_UNIVERSITY).isPresent()) {
             editPersonDescriptor
---
+++ src/main/java/seedu/address/model/person/GraduationDate.java
@@ -0,0 +1,83 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Represents the Graduation Date of a Person in the address book, as combination of a Month and Year
+ * Guarantees: immutable; is valid as declared in {@link #isValidYearMonth(String)}
+ */
+public class GraduationDate {
+
+    public static final String MESSAGE_CONSTRAINTS = "Graduation Date should be of the format year-month or month-year"
+        + " and adhere to the following constraints:\n"
+        + "1. year should contain only numbers, and is exactly 4 digit long\n"
+        + "2. month should contain only numbers, is exactly 2 digit long, and is a valid month number\n"
+        + "year and month values are separated by '-'";
+    public static final String YEAR_VALIDATION_REGEX = "\\d{4}";
+    public static final String MONTH_VALIDATION_REGEX = "^(1[0-2]|0[1-9])$";
+    public static final String VALIDATION_REGEX_MONTH_YEAR = String.format("%s-%s",
+            MONTH_VALIDATION_REGEX, YEAR_VALIDATION_REGEX);
+    public static final String VALIDATION_REGEX_YEAR_MONTH = String.format("%s-%s",
+            YEAR_VALIDATION_REGEX, MONTH_VALIDATION_REGEX);
+    public static final String YEAR_MONTH_PATTERN = "yyyy-MM";
+    public static final String MONTH_YEAR_PATTERN = "MM-yyyy";
+    public static final String DISPLAY_DATE_FORMAT = "MMM yyyy";
+
+    public final YearMonth value;
+
+    /**
+     * Constructs an {@code GraduationDate}.
+     *
+     * @param yearMonthString A valid graduation year month.
+     */
+    public GraduationDate(String yearMonthString) {
+        requireNonNull(yearMonthString);
+        checkArgument(isValidGraduationDate(yearMonthString));
+        value = YearMonth.parse(yearMonthString, getDateFormat(yearMonthString));
+    }
+
+    /**
+     * Returns if a given string is a valid graduation date.
+     */
+    public static boolean isValidGraduationDate(String test) {
+        return isPatternYearMonth(test) || isPatternMonthYear(test);
+    }
+
+    @Override
+    public String toString() {
+        return value.format(DateTimeFormatter.ofPattern(DISPLAY_DATE_FORMAT));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof GraduationDate // instanceof handles nulls
+                && value.equals(((GraduationDate) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    private static DateTimeFormatter getDateFormat(String yearMonthString) {
+        DateTimeFormatter dateFormat;
+        if (isPatternMonthYear(yearMonthString)) {
+            dateFormat = DateTimeFormatter.ofPattern(MONTH_YEAR_PATTERN);
+        } else {
+            dateFormat = DateTimeFormatter.ofPattern(YEAR_MONTH_PATTERN);
+        }
+        return dateFormat;
+    }
+
+    private static boolean isPatternMonthYear(String test) {
+        return test.matches(VALIDATION_REGEX_MONTH_YEAR);
+    }
+
+    private static boolean isPatternYearMonth(String test) {
+        return test.matches(VALIDATION_REGEX_YEAR_MONTH);
+    }
+}
